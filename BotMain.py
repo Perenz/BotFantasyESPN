e417850cb3397bafcb98b4b41b940a851b8f55b7
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-10-31 15:29:59 +0100

    Working bot with last night stats of all the players in myPlayers

--- BotMain.py
+++ BotMain.py
@@ -1,6 +1,30 @@
-import os
-import FantaInformation
+import telebot
+from datetime import timedelta
+from FantaInformation import *
 
 teleBotToken = os.environ['TELETOKEN']
 
-print(teleBotToken)
+bot = telebot.TeleBot(teleBotToken)
+
+myPlayers = []
+myPlayers.append(newPlayer('John', 'Collins', 'ATL'))
+myPlayers.append(newPlayer('Dwight', 'Howard', 'LAL'))
+myPlayers.append(newPlayer('Anthony', 'Davis', 'LAL'))
+myPlayers.append(newPlayer('Rudy', 'Gobert', 'UTA'))
+
+@bot.message_handler(commands=['today', 'night'])
+def send_welcome(message):
+    date = datetime.now() - timedelta(days=1)
+    date = date.strftime('%Y-%b-%d').upper()
+    response = toStringTeamStats(date,myPlayers)
+    #print(response)
+    bot.reply_to(message, response)
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+        response = 'Prova'
+        bot.reply_to(message, response)
+
+
+bot.polling()
--- FantaInformation.py
+++ FantaInformation.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import os
 
 apiKey = os.environ.get("NBAKEY")
-print(apiKey)
 
 #SportsDataIO
 
@@ -12,12 +11,12 @@ def todayGames():
     Return a list of games scheduled for tonight contaiting interested keys
     'GameID', 'AwayTeam', 'HomeTeam', 'AwayTeamScore', 'HomeTeamScore', 'IsClosed'
     '''
-    now = "2019-OCT-29"
+    date = datetime.now().strftime('%Y-%b-%d').upper()
 
     #Send http request to get info about games
     #https://api.sportsdata.io/v3/nba/scores/json/GamesByDate/{date}
     response = requests.get(
-        url="https://api.sportsdata.io/v3/nba/scores/json/GamesByDate/" + now,
+        url="https://api.sportsdata.io/v3/nba/scores/json/GamesByDate/" + date,
         headers={'Ocp-Apim-Subscription-Key':apiKey}
     )
 
@@ -31,22 +30,25 @@ def todayGames():
     #Return a list of dict containing interested keys    
     return toRtnGames
 
-def printTodayGames(games):
+def toStringTodayGames(games):
     '''
     print a list of games in a readable form
 
     Parameters:
         games: list of dict with keys:['GameID', 'AwayTeam', 'HomeTeam', 'AwayTeamScore', 'HomeTeamScore', 'IsClosed']
     '''
+
+    toRtn = ''
     for game in games:
         gameId = game["GameID"]
         #IsClosed = false -> Game not ended
         #IsClosed = true -> game ended -> Print the final score
         if(not game['IsClosed']):
-            print(f'{game["AwayTeam"]} @ {game["HomeTeam"]}')
+            toRtn += f'{game["AwayTeam"]} @ {game["HomeTeam"]}\n'
         else:
-            print(f'{game["AwayTeam"]} @ {game["HomeTeam"]}\n\t{game["AwayTeamScore"]} - {game["HomeTeamScore"]}')
-        print(f'\tGameID: {gameId}')
+            toRtn += f'{game["AwayTeam"]} @ {game["HomeTeam"]}\n\t{game["AwayTeamScore"]} - {game["HomeTeamScore"]}\n'
+        toRtn += f'\tGameID: {gameId}\n'
+    return toRtn
 
 
 def newPlayer(firstName, lastName, team):
@@ -75,28 +77,93 @@ def newPlayer(firstName, lastName, team):
         print("Errore nel cercare il giocatore\nAssicurarsi di aver inserito un cognome valido")
     return {'name': firstName + ' ' + lastName, 'team':team, 'playerID':playerID}
 
-def printPlayer(player):
+def toStringPlayer(player):
     '''
         Print a player dict in a readable form
         
         parameters
             -player dict containing ['name', 'team'] keys
     '''
-    print(f"{player['name']} with ID {player['playerID']}, plays for {player['team']} ")
+    return f"{player['name']} with ID {player['playerID']}, plays for {player['team']}\n"
 
-def printTeam(players):
+def toStringTeam(players):
     '''
         Print a players list in a readable form
 
         parameters
             -players list containing players dict
     '''
+    toRtn = ''
     for player in players:
-        printPlayer(player)
+        toRtn += toStringPlayer(player)
+
+    return toRtn
+
+def getStatsByDatePlayer(date, playerID):   
+    '''
+    Returns Player Stats by date and playerID
+    https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsByPlayer/DATE/PLAYEID?key=keyapiKey
+
+    Stats I'm interested in:
+        ESPN names: FG%, FT%, 3PM, REB, AST, STL, BLK, TO, DD, TD, PTS
+        SportsDataIO names: ['FieldGoalsPercentage', 'FreeThrowsPercentage', 'ThreePointersMade', 'Rebounds', 'Assists', 'Steals', 'BlockedShots',
+            'Turnovers', 'DoubleDoubles', 'TripleDoubles', 'Points']
+
+        DD and TD = 0.0 OR 1.0
+
+    parameters
+        - date: format sample: 2019-OCT-29
+        - playerID
+    '''
+
+    myKeys = {'FieldGoalsPercentage':'FG%', 'FreeThrowsPercentage':'FT%', 'ThreePointersMade':'3PM', 'Rebounds':'REB', 'Assists':'AST', 'Steals':'STL', 'BlockedShots':'BLK',
+            'Turnovers':'TO ', 'DoubleDoubles':'DD ', 'TripleDoubles':'TD ', 'Points':'PTS'}
+
+    #Return empty string if playerID does not play in that DATE
+    #https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsByPlayer/DATE/PLAYEID?key=keyapiKey
+    response = requests.get(
+        url = 'https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsByPlayer/{}/{}'.format(date, playerID), #Collins
+        headers={'Ocp-Apim-Subscription-Key':apiKey}   
+    )
+
+    #print(response.json().keys())
+
+    #Check if the playerID played on that date
+    playerStats = ''
+    if response.text != '':
+        jsonResp = response.json()
+        playerStats = {}
+        playerStats['Name'], playerStats['playerID'] = jsonResp['Name'], playerID
+        for key in myKeys.keys():
+            playerStats[myKeys[key]] = jsonResp[key]
+    
+    return playerStats
+    
+def toStringPlayerStats(playerStats):
+    toRtn = ''
+    if(playerStats!=''):
+        for key in playerStats.keys():
+            if key != 'playerID':
+                toRtn += "{:<15} {:<15}\n".format(key, playerStats[key])
+    return toRtn
+
+def toStringTeamStats(date, players):
+    toRtn = ' '
+    for player in players:
+        toRtn += toStringPlayerStats(getStatsByDatePlayer(date, player['playerID']))
+
+    return toRtn
+
+
+
+
 
 def main():
+    date = datetime.now().strftime('%Y-%b-%d').upper()
+    #print(date)
+
     games = todayGames()
-    printTodayGames(games)
+    print(toStringTodayGames(games))
 
     #I want to keep those games that involve players in my Fantasy Team
     '''
@@ -109,35 +176,14 @@ def main():
     myPlayers.append(newPlayer('John', 'Collins', 'ATL'))
     myPlayers.append(newPlayer('Dwight', 'Howard', 'LAL'))
     myPlayers.append(newPlayer('Anthony', 'Davis', 'LAL'))
+    myPlayers.append(newPlayer('Rudy', 'Gobert', 'UTA'))
 
-    printTeam(myPlayers)
+    print(toStringTeam(myPlayers))
 
     #Set of teams that roster some of my players
     teamSet = set(player['team'] for player in myPlayers)
-
-    '''
-    I'm interested in my Player Stats by date and playerID
-    https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsByPlayer/DATE/PLAYEID?key=keyapiKey
-
-    Stats I'm interested in:
-        ESPN names: FG%, FT%, 3PM, REB, AST, STL, BLK, TO, DD, TD, PTS
-        SportsDataIO names: ['FieldGoalsPercentage', 'FreeThrowsPercentage', 'ThreePointersMade', 'Rebounds', 'Assists', 'Steals', 'BlockedShots',
-            'Turnovers', 'DoubleDoubles', 'TripleDoubles', 'Points']
-
-        DD and TD = 0.0 OR 1.0
-    '''
-
-    #Return empty string if playerID does not play in that DATE
-    #https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsByPlayer/DATE/PLAYEID?key=keyapiKey
-    response = requests.get(
-        url = 'https://api.sportsdata.io/v3/nba/stats/json/PlayerGameStatsByPlayer/{}/{}'.format('2019-OCT-29', 20001835), #Collins
-        headers={'Ocp-Apim-Subscription-Key':apiKey}   
-    )
-
-
-    print(response.json())
     
+    print(toStringTeamStats('2019-OCT-30', myPlayers))
 
 
-main()
-
+#main()
