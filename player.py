01046c915d9f1ae404b4dc452231d6536df20227
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-11-02 13:23:15 +0100

    Addes player's methods

--- player.py
+++ player.py
@@ -1,5 +1,11 @@
 class player:
+    '''
+    Class that handles the player'stats computation
+    '''
     def __init__(self, firstName, lastName, team):
         self.firstName = firstName
         self.lastName = lastName
         self.team=team
+
+    def __str__(self):
+        return self.firstName + ' ' + self.lastName + ', plays for ' + self.team
